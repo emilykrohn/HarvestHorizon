1e8ad70a2340ad5f579ee96008f9b98c14845c2e
Emily Krohn <emilykrohn2@gmail.com>
2023-12-03 19:06:15 -0800

    Add walking in 4 directions

--- Scripts/Game/player.gd
+++ Scripts/Game/player.gd
@@ -1,25 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 75.0
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var horizontal_direction = Input.get_axis("ui_left", "ui_right")
+	var vertical_direction = Input.get_axis("ui_up", "ui_down")
+	if horizontal_direction or vertical_direction:
+		velocity = Vector2(horizontal_direction, vertical_direction).normalized() * SPEED
+		animated_sprite_2d.play("ForwardWalk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	animated_sprite_2d.play("ForwardIdle")
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		animated_sprite_2d.play("ForwardIdle")
 
 	move_and_slide()
