c4df16a78f2aa8f1aa054754b40eb3d44315d342
Emily Krohn <emilykrohn2@gmail.com>
2023-12-04 21:34:30 -0800

    When left click on dirt, play till animation

--- Scripts/Game/Map.gd
+++ Scripts/Game/Map.gd
@@ -1,6 +1,7 @@
 extends TileMap
 
 var can_select := false
+var can_till := false
 var previous_tile = false
 
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +17,10 @@ func _process(delta):
 			erase_cell(4, previous_tile)
 		set_cell(4, tile, 10, Vector2i(0,0), 0)
 		previous_tile = tile
+		if Input.is_action_pressed("left_click"):
+			$%Player.play_animation("ForwardTill")
+		elif Input.is_action_just_released("left_click"):
+			$%Player.stop_till_animation()
 
 func _on_upper_dirt_patch_mouse_entered():
 	can_select = true
@@ -32,3 +37,4 @@ func _on_lower_dirt_patch_mouse_exited():
 	can_select = false
 	erase_cell(4, previous_tile)
 	previous_tile = false
+
--- Scripts/Game/player.gd
+++ Scripts/Game/player.gd
@@ -3,29 +3,43 @@ extends CharacterBody2D
 
 const SPEED = 75.0
 var direction := "ForwardIdle"
+var is_tilling := false
+var current_animation
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
 	var horizontal_direction = Input.get_axis("ui_left", "ui_right")
 	var vertical_direction = Input.get_axis("ui_up", "ui_down")
-	if horizontal_direction or vertical_direction:
-		velocity = Vector2(horizontal_direction, vertical_direction).normalized() * SPEED
-		if horizontal_direction < 0:
-			animated_sprite_2d.play("LeftWalk")
-			direction = "LeftIdle"
-		elif horizontal_direction > 0:
-			animated_sprite_2d.play("RightWalk")
-			direction = "RightIdle"
-		elif vertical_direction < 0:
-			animated_sprite_2d.play("UpWalk")
-			direction = "UpIdle"
-		else:
-			animated_sprite_2d.play("ForwardWalk")
-			direction = "ForwardIdle"
-	else:
+	if is_tilling:
+		animated_sprite_2d.play("ForwardTill")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-		animated_sprite_2d.play(direction)
+	else:
+		if horizontal_direction or vertical_direction:
+			velocity = Vector2(horizontal_direction, vertical_direction).normalized() * SPEED
+			if horizontal_direction < 0:
+				animated_sprite_2d.play("LeftWalk")
+				direction = "LeftIdle"
+			elif horizontal_direction > 0:
+				animated_sprite_2d.play("RightWalk")
+				direction = "RightIdle"
+			elif vertical_direction < 0:
+				animated_sprite_2d.play("UpWalk")
+				direction = "UpIdle"
+			else:
+				animated_sprite_2d.play("ForwardWalk")
+				direction = "ForwardIdle"
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+			animated_sprite_2d.play(direction)
 
 	move_and_slide()
+
+func play_animation(animation):
+	is_tilling = true
+	current_animation = animation
+
+func stop_till_animation():
+	is_tilling = false
