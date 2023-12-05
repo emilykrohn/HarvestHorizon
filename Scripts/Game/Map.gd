7a4cbb1c4d12139469d58146f3bd925c1408bc87
Emily Krohn <emilykrohn2@gmail.com>
2023-12-04 16:51:52 -0800

    Erases rectangle tiles mouse isn't hovering over
    Only one tile is being selected at a time

--- Scripts/Game/Map.gd
+++ Scripts/Game/Map.gd
@@ -1,6 +1,7 @@
 extends TileMap
 
 var can_select := false
+var previous_tile = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,10 +10,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	if can_select:
 		var tile = local_to_map(get_global_mouse_position())
+		if previous_tile: 
+			erase_cell(4, previous_tile)
 		set_cell(4, tile, 10, Vector2i(0,0), 0)
+		previous_tile = tile
 
 func _on_upper_dirt_patch_mouse_entered():
 	can_select = true
@@ -22,6 +25,10 @@ func _on_lower_dirt_patch_mouse_entered():
 
 func _on_upper_dirt_patch_mouse_exited():
 	can_select = false
+	erase_cell(4, previous_tile)
+	previous_tile = false
 
 func _on_lower_dirt_patch_mouse_exited():
 	can_select = false
+	erase_cell(4, previous_tile)
+	previous_tile = false
