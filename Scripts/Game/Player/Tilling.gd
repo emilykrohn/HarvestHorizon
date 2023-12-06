8cf10a23fb5a3669cb651d6b38dc2cad07f2659c
Emily Krohn <emilykrohn2@gmail.com>
2023-12-06 09:12:25 -0800

    Remove cooldown on tilling
    Waiting for entire animation to play acts as cooldown system, so don't need this one anymore

--- Scripts/Game/Player/Tilling.gd
+++ Scripts/Game/Player/Tilling.gd
@@ -3,7 +3,6 @@ extends Node
 var current_tile
 var current_player_tile
 var is_tilling
-var can_till := true
 var play_animation := false
 
 func start_tilling(tile, player_tile):
@@ -23,15 +22,6 @@ func get_current_player_tile():
 
 func get_is_tilling():
 	return is_tilling
-
-func _on_tilling_cooldown_timeout():
-	can_till = true
-
-func set_can_till(flag):
-	can_till = flag
-
-func get_can_till():
-	return can_till
 
 func get_play_animation():
 	return play_animation
