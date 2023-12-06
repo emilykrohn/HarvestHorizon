9265343aac021e1bfcbd0ca9b450c5ab57502e72
Emily Krohn <emilykrohn2@gmail.com>
2023-12-06 09:26:10 -0800

    Player plants seeds when right clicks
    Makes sure that player is in range to plant

--- Scripts/Game/Map.gd
+++ Scripts/Game/Map.gd
@@ -15,12 +15,14 @@ func _process(delta):
 		# create select sprite where mouse is
 		set_cell(4, tile, 10, Vector2i(0,0), 0)
 		previous_tile = tile
-		if Input.is_action_pressed("left_click") and abs(tile - player_tile) <= Vector2i(1,1):
+		if Input.is_action_pressed("left_click") and in_range(tile, player_tile, 1):
 			$%Player/Tilling.start_tilling(tile, player_tile)
 			set_cell(5, tile, 11, Vector2i(5,1), 0)
 			tilled_tiles.append(tile)
 		elif Input.is_action_just_released("left_click"):
 			$%Player/Tilling.stop_tilling()
+		elif Input.is_action_pressed("right_click") and in_range(tile, player_tile, 1):
+			set_cell(5, tile, 13, Vector2i(0,1), 0)
 
 func _on_upper_dirt_patch_mouse_entered():
 	can_select = true
@@ -37,4 +39,7 @@ func _on_lower_dirt_patch_mouse_exited():
 	can_select = false
 	erase_cell(4, previous_tile)
 	previous_tile = false
+
+func in_range(tile, player_tile, range):
+	return abs(tile - player_tile) <= Vector2i(range, range)
 
