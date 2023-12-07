0683ad6bbdf566ddd80ad4881ae5e7428548bb77
Emily Krohn <emilykrohn2@gmail.com>
2023-12-06 19:52:08 -0800

    Clicking and dragging bug for tilling
    When player clicks and drags, it doesn't till other tiles anymore. They have to individually click each tile to till them

--- Scripts/Game/Map.gd
+++ Scripts/Game/Map.gd
@@ -15,7 +15,7 @@ func _process(delta):
 		# create select sprite where mouse is
 		set_cell(4, tile, 10, Vector2i(0,0), 0)
 		previous_tile = tile
-		if Input.is_action_pressed("left_click") and in_range(tile, player_tile, 1):
+		if Input.is_action_just_pressed("left_click") and in_range(tile, player_tile, 1):
 			$%Player/Tilling.start_tilling(tile, player_tile)
 			set_cell(5, tile, 11, Vector2i(5,1), 0)
 			tilled_tiles.append(tile)
